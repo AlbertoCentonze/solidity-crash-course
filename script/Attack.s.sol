415764041bab0f1cc68075016a7ffb37ee4dc358
Alberto Centonze <albicento.ac@gmail.com>
2022-10-24 15:59:08 +0200

    Completed reentrancy vulnerable contracts

--- script/Attack.s.sol
+++ script/Attack.s.sol
@@ -2,14 +2,17 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
+import "../src/Attack.sol";
 
 contract AttackScript is Script {
     address myAddress = 0xa4049faab47BbD7fdbA8783C7b64CC6b9c7d5920;
+		address contractToAttack = 0x884Fe06fB08100a3d1Ffcc6405a319514fc1285A;
 
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast(myAddress);
-				new 
+				Attack attack = new Attack(contractToAttack);
+				attack.attack{value: 1 ether}();
     }
 }
--- script/DeployReentrant.s.sol
+++ script/DeployReentrant.s.sol
@@ -8,7 +8,8 @@ contract DeployReentrant is Script {
     address myAddress = 0xa4049faab47BbD7fdbA8783C7b64CC6b9c7d5920;
 
     function run() public {
-        vm.startBroadcast();
-        new EtherStore();
+        vm.startBroadcast(myAddress);
+        EtherStore store = new EtherStore();
+				store.deposit{value: 2 ether}();
     }
 }
