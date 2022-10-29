59676f4cb27a7905fd82c704708588e28baf3b0a
pierreVinet <pierre.vinet17@gmail.com>
2022-10-29 12:09:49 +0200

    show nft

--- frontend/index.js
+++ frontend/index.js
@@ -1,16 +1,19 @@
 import { ethers } from "./ethers-5.6.esm.min.js"    //module
-import { contractAddress, abi } from "./constants.js"
+import { contractAddress, contractAddress2, abi, abiERC721, abiNft } from "./constants.js"
 
 const connectButton = document.getElementById('connectButton');
 const buyTicketButton = document.getElementById('buyTicketButton');
 const balanceButton = document.getElementById('balanceButton');
 const balanceETH = document.getElementById('balanceETH')
+const nftMinted = document.getElementById('nftMinted')
 connectButton.onclick = connect;
 buyTicketButton.onclick = buyTicket;
 balanceButton.onclick = getBalance;
 
 const provider = new ethers.providers.Web3Provider(window.ethereum);
 const signer = provider.getSigner();
+const contract2 = new ethers.Contract(contractAddress2, abiERC721, provider);
+const contract3 = new ethers.Contract(contractAddress2, abiNft, signer)
 
 async function connect() {
     if (typeof window.ethereum !== 'undefined') {
@@ -21,10 +24,24 @@ async function connect() {
     }
 }
 
+contract2.on("Transfer", (from, to, value) => {
+    let info = {
+        from: from,
+        to: to,
+        value: value,
+    }
+    console.log(info);
+    const nft = contract3.tokenURI(2).then((result) => {
+        console.log(result)
+        nftMinted.src = result;
+    });
+})
+
 async function buyTicket() {
     if (typeof window.ethereum !== 'undefined') {
         const contract = new ethers.Contract(contractAddress, abi, signer);
-        const mint = await contract.buyTicket({ value: ethers.utils.parseEther("1") }); 
+        await contract.buyTicket({ value: ethers.utils.parseEther("1") }); 
+        console.log('minted')
     }
 }   
 
@@ -39,3 +56,4 @@ async function getBalance() {
         })
     }
 }
+
